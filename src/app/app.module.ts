790f9dd426dcc86e0e7d8065b7514551284a3ff8
Vitor Souza <vitorx1280@gmail.com>
2021-09-13 17:52:49 -0300

    feat: módulo general adicionado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GeneralModule } from './general';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GeneralModule
   ],
   providers: [],
   bootstrap: [AppComponent]
