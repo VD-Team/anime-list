366727581dbf95364c6cb10248fc5d44715e5876
Douglas Henrique de Souza Pereira <50143403+douglazhs@users.noreply.github.com>
2021-09-21 11:56:01 -0300

    feat: Implementação de formulário

---
+++ src/app/anime-form-background/anime-form-background.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anime-form-background',
+  templateUrl: './anime-form-background.component.html',
+  styleUrls: ['./anime-form-background.component.css']
+})
+export class AnimeFormBackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
