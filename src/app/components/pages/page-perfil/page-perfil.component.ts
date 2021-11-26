0d8f205a22e26e981ac79c552327c7f881b12520
Douglas Henrique de Souza Pereira <50143403+douglazhs@users.noreply.github.com>
2021-11-26 01:50:59 -0300

    feat: implementação do login e cadastro do usuário

--- src/app/components/pages/page-perfil/page-perfil.component.ts
+++ src/app/components/pages/page-perfil/page-perfil.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Favorito } from 'src/app/anime-form/anime-form.component';
+import  axios  from 'axios';
 import { Anime } from '..';
 
 @Component({
@@ -10,53 +11,103 @@ import { Anime } from '..';
 export class PagePerfilComponent implements OnInit {
 
   user: User | undefined
-  username: string | null = null
-  inputText: string = ''
+  loginPassword: string = ''
+  loginEmail: string = ''
+
+  name: string = ''
+  email: string = ''
+  password: string = ''
+  rePassword: string = ''
+  confirmation: boolean = false
+  genre: string = ''
+
   canLogin: boolean = false
 
   //Sexo
-  defaultStatus = ['Masculino', 'Feminino', 'Não quero informar']
-  status: string = ''
+  defaultGenre = ['Masculino', 'Feminino', 'Não quero informar']
 
   constructor() { }
 
   ngOnInit(): void {
-    this.username = sessionStorage.getItem('username')
-    if(this.username != null) {
-      this.getUserInfo()
-    }
+    // this.username = sessionStorage.getItem('username')
+    // if(this.username != null) {
+    //   this.getUserInfo()
+    // }
   }
 
-  async getUser(): Promise<void> {
-    const username = this.username?.split(' ').join('-')
-    fetch(`https://api.github.com/users/${this.username}`)
-    .then(response => response.json())
-    .then(json => {
-      this.user = new User(json, this.username!)
-      localStorage.setItem(`user-${username}`, JSON.stringify(this.user))
-    })
-    .catch(err => console.log(err))
-  }
+  // async getUser(): Promise<void> {
+  //   const username = this.username?.split(' ').join('-')
+  //   fetch(`https://api.github.com/users/${this.username}`)
+  //   .then(response => response.json())
+  //   .then(json => {
+  //     this.user = new User(json, this.username!)
+  //     localStorage.setItem(`user-${username}`, JSON.stringify(this.user))
+  //   })
+  //   .catch(err => console.log(err))
+  // }
 
-  getUserInfo() {
-    const username = this.username?.split(' ').join('-')
-    let userData = localStorage.getItem(`user-${username}`)
-    if(userData == null) {
-      this.getUser()
-    } else {
-      this.user = new User(JSON.parse(userData), this.username!)
-    }
-  }
+  // getUserInfo() {
+  //   const username = this.username?.split(' ').join('-')
+  //   let userData = localStorage.getItem(`user-${username}`)
+  //   if(userData == null) {
+  //     this.getUser()
+  //   } else {
+  //     this.user = new User(JSON.parse(userData), this.username!)
+  //   }
+  // }
 
   logar() {
-    sessionStorage.setItem('username', this.inputText)
-    this.username = this.inputText
-    this.getUserInfo()
+    let user = {
+      email: this.loginEmail, 
+      password: this.loginPassword
+    }
+
+    axios.post('https://intense-dusk-81169.herokuapp.com/login', user)
+    .then (response => {
+      console.log(response)
+      if (response && !response.data.error){
+        this.loginEmail = ''
+        this.loginPassword = ''
+      }
+    })
+  }
+
+  registrar(){
+    let person = {
+      name: this.name, 
+      email: this.email, 
+      password: this.password,
+      genre: this.genre,
+      confirmation: this.confirmation
+    }
+
+    axios.post('https://intense-dusk-81169.herokuapp.com/users', person)
+    .then (response => {
+      console.log(response)
+      if (response && !response.data.error){
+        this.name = ''
+        this.confirmation = false
+        this.email = ''
+        this.genre = ''
+        this.password = ''
+        this.rePassword = ''
+      }
+    })
+  }
+
+  markCheckbox(){
+    console.log(this.confirmation)
+    this.confirmation = !this.confirmation
   }
 
   validateInput() {
-    this.canLogin = this.inputText.length >= 3
+    this.canLogin = this.name.length >= 3
   }
+
+  validateLogin(){
+    this.canLogin = this.loginEmail.length >= 3
+  }
+
 }
 
 export class User {
